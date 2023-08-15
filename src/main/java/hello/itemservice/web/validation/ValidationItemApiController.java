a084a15707d5c1ed95bef9194232a6a4eef8e64e
tavano2 <asjk158@naver.com>
2023-08-15 21:07:49 +0900

    Initial commit

---
+++ src/main/java/hello/itemservice/web/validation/ValidationItemApiController.java
@@ -0,0 +1,28 @@
+package hello.itemservice.web.validation;
+
+import hello.itemservice.web.validation.form.ItemSaveForm;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/validation/api/items")
+public class ValidationItemApiController {
+
+    @ResponseBody
+    @PostMapping("/add")
+    public Object addItem(@RequestBody @Validated ItemSaveForm form, BindingResult bindingResult) {
+        log.info("Api 컨트롤러 호출");
+
+        if (bindingResult.hasErrors()) {
+            log.info("검증 오류 발생 errors={}", bindingResult);
+            return bindingResult.getAllErrors();
+        }
+
+        log.info("검증 성공");
+        return form;
+    }
+
+}
